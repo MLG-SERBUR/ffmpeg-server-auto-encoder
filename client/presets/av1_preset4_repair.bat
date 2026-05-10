60a53afdc74de2b0a91e8a4bbbbb809eeeb26726
Server <19244210+MLG-SERBUR@users.noreply.github.com>
2026-05-10 14:18:51 -0700

    preset 4

--- client/presets/av1_preset4_repair.bat
+++ client/presets/av1_preset4_repair.bat
@@ -2,7 +2,7 @@
 REM AV1 preset for repairing VFR game captures (Game DVR broken timestamps)
 REM -fflags +genpts regenerates PTS, -fps_mode passthrough preserves VFR
 set "INPUT_OPTIONS=-fflags +genpts"
-set "VIDEO_ENCODER=-c:v libsvtav1 -preset 2 -crf 38 -fps_mode passthrough"
+set "VIDEO_ENCODER=-c:v libsvtav1 -preset 4 -crf 40 -fps_mode passthrough"
 set "AUDIO_ENCODER=-c:a libopus -b:a 96k -vbr on"
 set "OUTPUT_SUFFIX=-av1"
 set "FINAL_EXT=.mp4"
